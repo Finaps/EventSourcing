7a7fa7ccfd1ac9bf39ab2e797ed7e97c91283157
Bram Kraaijeveld <bram.kraaijeveld@finaps.nl>
2021-11-04 16:42:42 +0100

    format so tabs are 2 spaces

--- EventSourcing.Core.Tests/MockEventStore/MockAsyncQueryableProvider.cs
+++ EventSourcing.Core.Tests/MockEventStore/MockAsyncQueryableProvider.cs
@@ -8,53 +8,53 @@ using System.Threading.Tasks;
 
 namespace EventSourcing.Core.Tests.MockEventStore
 {
-    internal class MockAsyncQueryable<TResult> : IOrderedQueryable<TResult>, IAsyncEnumerable<TResult>
-        {
-            private readonly IQueryable<TResult> _queryable;
+  internal class MockAsyncQueryable<TResult> : IOrderedQueryable<TResult>, IAsyncEnumerable<TResult>
+  {
+    private readonly IQueryable<TResult> _queryable;
 
-            public MockAsyncQueryable(IQueryable<TResult> queryable)
-            {
-                _queryable = queryable;
-                Provider = new MockAsyncQueryableProvide(queryable.Provider);
-            }
-
-            public Type ElementType => typeof(TResult);
-
-            public Expression Expression => _queryable.Expression;
-            public IQueryProvider Provider { get; }
-
-
-            public IEnumerator<TResult> GetEnumerator() => _queryable.GetEnumerator();
-
-            IEnumerator IEnumerable.GetEnumerator() => _queryable.GetEnumerator();
-
-            public async IAsyncEnumerator<TResult> GetAsyncEnumerator(CancellationToken cancellationToken = default)
-            {
-                foreach (var item in _queryable.ToList())
-                {
-                    await Task.CompletedTask;
-                    yield return item;
-                }
-            }
-        
-    }
-    
-    internal class MockAsyncQueryableProvide : IQueryProvider
+    public MockAsyncQueryable(IQueryable<TResult> queryable)
     {
-        private readonly IQueryProvider _provider;
-        public MockAsyncQueryableProvide(IQueryProvider provider) =>
-            _provider = provider;
-
-        public IQueryable<TElement> CreateQuery<TElement>(Expression expression) =>
-            new MockAsyncQueryable<TElement>(_provider.CreateQuery<TElement>(expression));
-
-        public IQueryable CreateQuery(Expression expression) =>
-            CreateQuery<object>(expression);
-
-        public object Execute(Expression expression) =>
-            _provider.Execute(expression);
-
-        public TResult Execute<TResult>(Expression expression) =>
-            _provider.Execute<TResult>(expression);
+      _queryable = queryable;
+      Provider = new MockAsyncQueryableProvide(queryable.Provider);
     }
+
+    public Type ElementType => typeof(TResult);
+
+    public Expression Expression => _queryable.Expression;
+    public IQueryProvider Provider { get; }
+
+
+    public IEnumerator<TResult> GetEnumerator() => _queryable.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => _queryable.GetEnumerator();
+
+    public async IAsyncEnumerator<TResult> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+    {
+      foreach (var item in _queryable.ToList())
+      {
+        await Task.CompletedTask;
+        yield return item;
+      }
+    }
+  }
+
+  internal class MockAsyncQueryableProvide : IQueryProvider
+  {
+    private readonly IQueryProvider _provider;
+
+    public MockAsyncQueryableProvide(IQueryProvider provider) =>
+      _provider = provider;
+
+    public IQueryable<TElement> CreateQuery<TElement>(Expression expression) =>
+      new MockAsyncQueryable<TElement>(_provider.CreateQuery<TElement>(expression));
+
+    public IQueryable CreateQuery(Expression expression) =>
+      CreateQuery<object>(expression);
+
+    public object Execute(Expression expression) =>
+      _provider.Execute(expression);
+
+    public TResult Execute<TResult>(Expression expression) =>
+      _provider.Execute<TResult>(expression);
+  }
 }
