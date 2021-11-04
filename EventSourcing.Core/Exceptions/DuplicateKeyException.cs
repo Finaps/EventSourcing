7f9fe652259eaa079b7c94d412516d312d67ab17
Bram Kraaijeveld <bram.kraaijeveld@finaps.nl>
2021-11-04 16:39:42 +0100

    add error handling and associated tests

--- EventSourcing.Core/Exceptions/DuplicateKeyException.cs
+++ EventSourcing.Core/Exceptions/DuplicateKeyException.cs
@@ -9,5 +9,11 @@ namespace EventSourcing.Core.Exceptions
     public DuplicateKeyException(string message) : base(message) { }
     public DuplicateKeyException(string message, Exception inner) : base(message, inner) { }
     public DuplicateKeyException(IEnumerable<Exception> innerExceptions) : base(innerExceptions) { }
+
+    public static DuplicateKeyException CreateDuplicateIdException(Event e) => 
+      new($"Event '{e.Type}' with Id '{e.Id}' already exists for Aggregate '{e.AggregateType}' with Id '{e.AggregateId}'");
+    
+    public static DuplicateKeyException CreateDuplicateVersionException(Event e) =>
+      new($"Event '{e.Type}' with Version '{e.AggregateVersion}' already exists for Aggregate '{e.AggregateType}' with Id '{e.AggregateId}'");
   }
 }
