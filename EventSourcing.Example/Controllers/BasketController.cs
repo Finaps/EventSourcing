192821d209fb99fd6825fba573a77cff3589c8ca
Niels <niels.disveld@finaps.nl>
2021-12-07 14:47:25 +0100

    Added checkout controller method

--- EventSourcing.Example/Controllers/BasketController.cs
+++ EventSourcing.Example/Controllers/BasketController.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using EventSourcing.Core;
 using EventSourcing.Example.ComandBus;
 using EventSourcing.Example.Domain.Aggregates.Baskets;
 using EventSourcing.Example.Domain.Aggregates.Orders;
@@ -12,9 +14,11 @@ namespace EventSourcing.Example.Controllers
     public class BasketController : ControllerBase
     {
         private readonly ICommandBus _commandBus;
-        public BasketController(ICommandBus commandBus)
+        private readonly IAggregateService<Event> _aggregateService;
+        public BasketController(ICommandBus commandBus, IAggregateService<Event> aggregateService)
         {
             _commandBus = commandBus;
+            _aggregateService = aggregateService;
         }
         [HttpPost]
         public async Task<OkObjectResult> CreateBasket()
@@ -35,6 +39,35 @@ namespace EventSourcing.Example.Controllers
             var basket = await _commandBus.ExecuteCommand<Basket>(new AddProductToBasket(basketId, productId, amount));
             
             return Ok(basket);
+        }
+        
+        [HttpPost("{basketId:guid}/checkout")]
+        public async Task<ObjectResult> CheckoutBasket([FromRoute] Guid basketId)
+        {
+            var basket = await _aggregateService.RehydrateAsync<Basket>(basketId);
+            var successfullyCheckedOut = new List<Item>();
+
+            try
+            {
+                foreach (var item in basket.Items)
+                {
+                    await _commandBus.ExecuteCommand<Product>(new Purchase(item.ProductId, basketId,
+                            item.Quantity));
+                    successfullyCheckedOut.Add(item);
+                }
+
+                await _commandBus.ExecuteCommand<Basket>(new CheckoutBasket(basketId));
+            }
+            catch (Exception e)
+            {
+                // Rollback of all product purchases that were successful
+                foreach(var item in successfullyCheckedOut)
+                    await _commandBus.ExecuteCommand<Product>(new AddStock(item.ProductId, item.Quantity));
+                return BadRequest(e);
+            }
+            
+            var order = await _commandBus.ExecuteCommand<Order>(new CreateOrder(Guid.NewGuid(), basketId));
+            return Ok(order);
         }
     }
 }
