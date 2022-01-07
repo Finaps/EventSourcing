63c582fcc9b71a0eccc4bf84899fbf4ca8db47aa
Niels <niels.disveld@finaps.nl>
2022-01-07 13:35:35 +0100

    Implemented snapshot in ExampleProject

---
+++ EventSourcing.Example/Domain/Aggregates/Products/ProductSnapshot.cs
@@ -0,0 +1,6 @@
+using System.Collections.Generic;
+using EventSourcing.Core;
+
+namespace EventSourcing.Example.Domain.Aggregates.Products;
+
+public record ProductSnapshot(string Name, int Quantity, List<Reservation> Reservations) : SnapshotEvent();
