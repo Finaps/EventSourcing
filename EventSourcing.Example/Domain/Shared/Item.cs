32040f739d9ae853468c5cd345fa44fa519edda6
Niels <niels.disveld@finaps.nl>
2022-01-11 13:57:01 +0100

    Changed Item class to record

--- EventSourcing.Example/Domain/Shared/Item.cs
+++ EventSourcing.Example/Domain/Shared/Item.cs
@@ -1,15 +1,5 @@
 using System;
 
-namespace EventSourcing.Example.Domain.Shared
-{
-    public class Item
-    {
-        public Guid ProductId { get; }
-        public int Quantity { get; set; }
-        public Item(Guid productId, int quantity)
-        {
-            ProductId = productId;
-            Quantity = quantity;
-        }
-    }
-}
+namespace EventSourcing.Example.Domain.Shared;
+
+public record Item(Guid ProductId, int Quantity);
