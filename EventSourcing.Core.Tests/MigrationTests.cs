5c3061f1fe3e37e8ba1d57daff619f8c1e6b0547
Niels <niels.disveld@finaps.nl>
2022-01-14 14:02:48 +0100

    Added tests for trivial migrations

--- EventSourcing.Core.Tests/MigrationTests.cs
+++ EventSourcing.Core.Tests/MigrationTests.cs
@@ -1,3 +1,5 @@
+using System.IO;
+using System.Text.Json;
 using EventSourcing.Core.Tests.Mocks;
 
 namespace EventSourcing.Core.Tests;
@@ -34,5 +36,32 @@ public abstract class MigrationTests
         Assert.NotNull(rehydrated);
         Assert.Single(rehydrated.SomeIds);
         Assert.Equal(someId, rehydrated.SomeIds.Single());
+    }
+    
+    [Fact]
+    public void Can_Convert_Trivial_Migration()
+    {
+        var e = new TrivialMigrationEventOriginal(Guid.NewGuid(), "something", 42, 42.00m);
+        var aggregate = new MigratedAggregate();
+        aggregate.Add(e);
+        var converter = new EventConverter<Event>();
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        
+        using var writeStream = new MemoryStream();
+        converter.Write(new Utf8JsonWriter(writeStream), aggregate.UncommittedEvents.Single(), options);
+        writeStream.Position = 0;
+        var json = System.Text.Encoding.UTF8.GetString(writeStream.ToArray());
+        // Manipulate JSON string so that its type field has the correct type to convert to
+        var adjustedJson = json.Replace(nameof(TrivialMigrationEventOriginal), nameof(TrivialMigrationEvent));
+
+        var bytes = System.Text.Encoding.UTF8.GetBytes(adjustedJson);
+        var reader = new Utf8JsonReader(bytes, true, default);
+        var converted = converter.Read(ref reader, typeof(TrivialMigrationEvent), options) as TrivialMigrationEvent;
+            
+        Assert.NotNull(converted);
+        Assert.Equal(e.someId,converted.someId);
+        Assert.Equal(e.someString,converted.someString);
+        Assert.Equal(e.someInt,converted.someInt);
+        Assert.Null(converted.addedField);
     }
 }
--- EventSourcing.Core.Tests/Mocks/MigrationEvents.cs
+++ EventSourcing.Core.Tests/Mocks/MigrationEvents.cs
@@ -5,3 +5,5 @@ public record MigrationEvent(string someId): Event;
 public record MigrationEventV2(Guid? someId): Event;
 
 public record MigrationEventV3(List<Guid> someIds): Event;
+public record TrivialMigrationEventOriginal(Guid someId, string someString, int someInt, decimal removedField): Event;
+public record TrivialMigrationEvent(Guid someId, string someString, int? someInt, Guid? addedField): Event;
