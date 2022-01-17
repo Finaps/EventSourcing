26c950e5fc9153de88517fa00f64e4fee42cf32b
Niels <niels.disveld@finaps.nl>
2022-01-17 18:15:44 +0100

    Rebase

--- EventSourcing.Example/Domain/Aggregates/Products/ProductSnapshot.cs
+++ EventSourcing.Example/Domain/Aggregates/Products/ProductSnapshot.cs
@@ -3,4 +3,4 @@ using EventSourcing.Core;
 
 namespace EventSourcing.Example.Domain.Aggregates.Products;
 
-public record ProductSnapshot(string Name, int Quantity, List<Reservation> Reservations) : SnapshotEvent();
+public record ProductSnapshot(string Name, int Quantity, List<Reservation> Reservations) : Snapshot;
