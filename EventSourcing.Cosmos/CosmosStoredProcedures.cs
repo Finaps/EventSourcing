9b86d34ddacdf67a2ff1fe84f6b8fcf170e7c3fc
nielsdisveld <Niels.Disveld@finaps.nl>
2022-03-02 10:59:49 +0100

    DeleteAggregateAll js function as a static string

--- EventSourcing.Cosmos/CosmosStoredProcedures.cs
+++ EventSourcing.Cosmos/CosmosStoredProcedures.cs
@@ -6,23 +6,27 @@ namespace EventSourcing.Cosmos;
 
 public static class CosmosStoredProcedures
 {
-    private const string BulkDeleteId = "BulkDelete";
+    private const string BulkDeleteId = "DeleteAggregateAll";
 
     public static async Task CreateBulkDeleteProcedure(this Container container)
     {
         await TryDeleteStoredProcedure(container, BulkDeleteId);
-        var body = await File.ReadAllTextAsync(@"../../../../EventSourcing.Cosmos/StoredProcedures/BulkDelete.js");
-        await CreateStoredProcedure(container, BulkDeleteId, body);
+        await CreateStoredProcedure(container, BulkDeleteId, StoredProcedures.DeleteAggregateAll);
     }
     public static async Task<int> ExecuteBulkDeleteProcedure(this Container container, Guid partitionId, Guid aggregateId)
     {
-        var query = $"SELECT * FROM {container.Id} e WHERE e.AggregateId = '{aggregateId}' AND e.PartitionId = '{partitionId}' AND e.Kind = {(int) RecordKind.Event}";
+        var deleted = 0;
         StoredProcedureExecuteResponse<BulkDeleteResponse> response;
+        
         do
+        {
             response = await container.Scripts.ExecuteStoredProcedureAsync<BulkDeleteResponse>(BulkDeleteId,
-                new PartitionKey($"{partitionId}"), new dynamic[] { query });
-        while (response.Resource.continuation);
-        return response.Resource.deleted;
+                new PartitionKey($"{partitionId}"),
+                new dynamic[] { container.Id, partitionId.ToString(), aggregateId.ToString() });
+            deleted += response.Resource.deleted;
+        } while (response.Resource.continuation);
+        
+        return deleted;
     }
     
     private static async Task CreateStoredProcedure(this Container container, string scriptId, string body) => 
