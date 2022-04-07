76155dc1951e07ccd16ab02380723535a81ed8a7
nielsdisveld <Niels.Disveld@finaps.nl>
2022-04-07 16:46:11 +0200

    Added tests for AddEvent, AddSnapshot and DeleteAllEvents methods on Transaction

--- EventSourcing.Core.Tests/RecordTransactionTests/AddSnapshot.cs
+++ EventSourcing.Core.Tests/RecordTransactionTests/AddSnapshot.cs
@@ -2,4 +2,39 @@ namespace EventSourcing.Core.Tests;
 
 public abstract partial class EventSourcingTests
 {
+    [Fact]
+    public async Task RecordTransaction_AddSnapshot_Can_Add_Snapshot_In_Transaction()
+    {
+        var s = new Snapshot { AggregateId = Guid.NewGuid(), AggregateType = nameof(EmptyAggregate) };
+        
+        await RecordStore.CreateTransaction()
+            .AddSnapshot(s)
+            .CommitAsync();
+
+        var count = await RecordStore
+            .GetSnapshots<EmptyAggregate>()
+            .Where(x => x.AggregateId == s.AggregateId)
+            .AsAsyncEnumerable()
+            .CountAsync();
+    
+        Assert.Equal(1, count);
+    }
+    
+    [Fact]
+    public async Task RecordTransaction_AddSnapshot_Cannot_Add_Invalid_Snapshot_In_Transaction()
+    {
+        var s = new Snapshot { AggregateType = nameof(EmptyAggregate) };
+        
+        await Assert.ThrowsAsync<RecordValidationException>(async () => await RecordStore.CreateTransaction()
+            .AddSnapshot(s)
+            .CommitAsync());
+
+        var count = await RecordStore
+            .GetSnapshots<EmptyAggregate>()
+            .Where(x => x.AggregateId == s.AggregateId)
+            .AsAsyncEnumerable()
+            .CountAsync();
+    
+        Assert.Equal(0, count);
+    }
 }
--- EventSourcing.Core.Tests/RecordTransactionTests/DeleteAllEvents.cs
+++ EventSourcing.Core.Tests/RecordTransactionTests/DeleteAllEvents.cs
@@ -2,4 +2,33 @@ namespace EventSourcing.Core.Tests;
 
 public abstract partial class EventSourcingTests
 {
+    [Fact]
+    public async Task RecordTransaction_AddSnapshot_Can_Delete_All_Event_In_Transaction()
+    {
+        var aggregateId = Guid.NewGuid();
+        
+        var events = Enumerable.Range(0, 5)
+            .Select<int, Event>( i => new EmptyEvent
+            {
+                AggregateId = aggregateId,
+                AggregateType = nameof(EmptyAggregate),
+                Index = i
+            }).ToList();
+
+        await RecordStore.CreateTransaction()
+          .AddEvents(events)
+          .CommitAsync();
+        
+        await RecordStore.CreateTransaction()
+            .DeleteAllEvents<EmptyAggregate>(aggregateId, events.Count)
+            .CommitAsync();
+
+        var count = await RecordStore
+            .GetEvents<EmptyAggregate>()
+            .Where(x => x.AggregateId == aggregateId)
+            .AsAsyncEnumerable()
+            .CountAsync();
+        
+        Assert.Equal(0, count);
+    }
 }
