e9374346d8a032f3e8b66202a7b841bbb07e2802
nielsdisveld <Niels.Disveld@finaps.nl>
2022-04-15 12:12:16 +0200

    Added tests for RecordTransaction

--- EventSourcing.Core.Tests/RecordTransactionTests/AddSnapshot.cs
+++ EventSourcing.Core.Tests/RecordTransactionTests/AddSnapshot.cs
@@ -5,34 +5,36 @@ public abstract partial class EventSourcingTests
     [Fact]
     public async Task RecordTransaction_AddSnapshot_Can_Add_Snapshot_In_Transaction()
     {
-        var e = new SnapshotEvent { AggregateId = Guid.NewGuid() };
-        var s = new SnapshotSnapshot { AggregateId = e.AggregateId };
+        var e = new SnapshotEvent
+            { AggregateId = Guid.NewGuid(), AggregateType = nameof(SnapshotAggregate), Index = 0 };
+        var s = new SnapshotSnapshot 
+            { AggregateId = e.AggregateId, AggregateType = nameof(SnapshotAggregate), Index = 0, Counter = 10};
         
         await RecordStore.CreateTransaction()
-            .AddEvents(new List<Event> { e })
+            .AddEvents(new Event[] { e })
             .AddSnapshot(s)
             .CommitAsync();
 
-        var count = await RecordStore
+        var snapshot = await RecordStore
             .GetSnapshots<SnapshotAggregate>()
             .Where(x => x.AggregateId == s.AggregateId)
             .AsAsyncEnumerable()
-            .CountAsync();
-    
-        Assert.Equal(1, count);
+            .SingleAsync() as SnapshotSnapshot;
+        
+        Assert.NotNull(snapshot);
+        Assert.Equal(s.Counter, snapshot!.Counter);
     }
     
     [Fact]
     public async Task RecordTransaction_AddSnapshot_Cannot_Add_Invalid_Snapshot_In_Transaction()
     {
-        var s = new Snapshot { AggregateType = nameof(EmptyAggregate) };
-        
-        await Assert.ThrowsAsync<RecordValidationException>(async () => await RecordStore.CreateTransaction()
-            .AddSnapshot(s)
-            .CommitAsync());
+        var s = new SnapshotSnapshot { AggregateId = Guid.NewGuid(), AggregateType = nameof(SnapshotAggregate) , Index = -1};
+
+        var transaction = RecordStore.CreateTransaction();
+        Assert.Throws<RecordValidationException>(() => transaction.AddSnapshot(s));
 
         var count = await RecordStore
-            .GetSnapshots<EmptyAggregate>()
+            .GetSnapshots<SnapshotAggregate>()
             .Where(x => x.AggregateId == s.AggregateId)
             .AsAsyncEnumerable()
             .CountAsync();
--- EventSourcing.Core.Tests/RecordTransactionTests/UpsertProjection.cs
+++ EventSourcing.Core.Tests/RecordTransactionTests/UpsertProjection.cs
@@ -2,4 +2,34 @@ namespace EventSourcing.Core.Tests;
 
 public abstract partial class EventSourcingTests
 {
+    [Fact]
+    public async Task RecordTransaction_UpsertProjection_Can_Upsert_Projection_In_Transaction()
+    {
+        var e = new EmptyEvent();
+        var a = new EmptyAggregate();
+        a.Apply(e);
+        await AggregateService.PersistAsync(a);
+
+        var projectionBefore = await RecordStore
+            .GetProjections<EmptyProjection>()
+            .Where(x => x.AggregateId == a.Id)
+            .AsAsyncEnumerable()
+            .SingleAsync();
+        
+        var updatedProjection = projectionBefore with { Timestamp = DateTimeOffset.Now };
+        
+        Assert.NotEqual(projectionBefore.Timestamp, updatedProjection.Timestamp);
+        
+        await RecordStore.CreateTransaction()
+            .UpsertProjection(updatedProjection)
+            .CommitAsync();
+
+        var result = await RecordStore
+            .GetProjections<EmptyProjection>()
+            .Where(x => x.AggregateId == a.Id)
+            .AsAsyncEnumerable()
+            .SingleAsync();
+    
+        Assert.Equal(updatedProjection, result);
+    }
 }
