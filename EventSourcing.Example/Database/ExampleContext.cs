9e68837988a4bf8d288290f26d7946db06559d41
nielsdisveld <Niels.Disveld@finaps.nl>
2022-05-09 17:01:46 +0200

    Added IDesignTimeDbContextFactory to ExampleContext

--- EventSourcing.Example/Database/ExampleContext.cs
+++ EventSourcing.Example/Database/ExampleContext.cs
@@ -1,6 +1,8 @@
 using Finaps.EventSourcing.EF;
 using Finaps.EventSourcing.Example.Domain.Products;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace Finaps.EventSourcing.Example.Infrastructure;
 
@@ -19,5 +21,24 @@ public class ExampleContext : RecordContext
         {
             entity.OwnsMany(x => x.Reservations);
         });
+    }
+}
+
+public class TestContextFactory : IDesignTimeDbContextFactory<ExampleContext>
+{
+    public ExampleContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", false)
+            .AddJsonFile("appsettings.local.json", true)
+            .AddEnvironmentVariables()
+            .Build();
+    
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+        return new ExampleContext(new DbContextOptionsBuilder<ExampleContext>()
+            .UseNpgsql(configuration.GetConnectionString("RecordStore"))
+            .EnableSensitiveDataLogging()
+            .Options);
     }
 }
