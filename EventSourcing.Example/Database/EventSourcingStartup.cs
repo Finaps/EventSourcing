bf9371bb27e9a52e2c015b7500069c72e0d051b5
nielsdisveld <Niels.Disveld@finaps.nl>
2022-05-11 16:51:59 +0200

    Update docs

--- EventSourcing.Example/Database/EventSourcingStartup.cs
+++ EventSourcing.Example/Database/EventSourcingStartup.cs
@@ -10,19 +10,20 @@ public static class EventSourcingStartup
 {
     public static IServiceCollection AddEventSourcing(this IServiceCollection services, IConfigurationRoot configuration)
     {
-        if (configuration.GetConnectionString("RecordStore") is { } postgresConnectionString)
+        if (configuration.GetConnectionString("RecordStore") is not null)
         {
             services.AddDbContext<RecordContext, ExampleContext>(options =>
             {
-                options.UseNpgsql(postgresConnectionString);
+                options.UseNpgsql(configuration.GetConnectionString("RecordStore"));
             });
             services.AddScoped<IRecordStore, EntityFrameworkRecordStore>();
         }
-        else
+        else if (configuration.GetSection("Cosmos").Exists())
         {
             services.Configure<CosmosRecordStoreOptions>(configuration.GetSection("Cosmos"));
             services.AddSingleton<IRecordStore, CosmosRecordStore>();
         }
+        else throw new ArgumentException("No configuration found for event store", nameof(configuration));
         
         return services.AddScoped<IAggregateService, AggregateService>();
     }
