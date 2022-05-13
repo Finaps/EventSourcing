95c5f135251232a942bb707dcd9d8b24e0f54c52
nielsdisveld <Niels.Disveld@finaps.nl>
2022-05-13 15:36:17 +0200

    Set UseCosmos env var in example projects

--- EventSourcing.Example.Tests.Cosmos/CosmosTestServer.cs
+++ EventSourcing.Example.Tests.Cosmos/CosmosTestServer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Reflection;
 using Finaps.EventSourcing.Example;
@@ -11,6 +12,7 @@ public static class CosmosTestServer
 {
     public static TestServer GetServer()
     {
+      Environment.SetEnvironmentVariable("UseCosmos", "true");
       var path = Assembly.GetAssembly(typeof(CosmosTestServer))?.Location;
       var hostBuilder = new WebHostBuilder()
         .UseContentRoot(Path.GetDirectoryName(path)!)
