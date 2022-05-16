946ff962c3b83c7492a887cd66881abdb8984b5d
nielsdisveld <Niels.Disveld@finaps.nl>
2022-05-16 12:09:49 +0200

    Improve EventSourcingStartup

--- EventSourcing.Example/Database/EventSourcingStartup.cs
+++ EventSourcing.Example/Database/EventSourcingStartup.cs
@@ -8,23 +8,23 @@ namespace Finaps.EventSourcing.Example.Infrastructure;
 
 public static class EventSourcingStartup
 {
-    public static IServiceCollection AddEventSourcing(this IServiceCollection services, IConfigurationRoot configuration)
+    public static IServiceCollection StartupEventSourcing(this IServiceCollection services, IConfigurationRoot configuration)
     {
         if (configuration.GetValue<bool>("UseCosmos") && configuration.GetSection("Cosmos").Exists())
-        {
-            services.Configure<CosmosRecordStoreOptions>(configuration.GetSection("Cosmos"));
-            services.AddSingleton<IRecordStore, CosmosRecordStore>();
-        }
+            // Call AddEventSourcing from the EventSourcing.Cosmos module
+            services.AddEventSourcing(configuration.GetSection("Cosmos"));
         else if (configuration.GetConnectionString("RecordStore") is not null)
         {
+            // Configure DbContext
             services.AddDbContext<RecordContext, ExampleContext>(options =>
             {
                 options.UseNpgsql(configuration.GetConnectionString("RecordStore"));
             });
-            services.AddScoped<IRecordStore, EntityFrameworkRecordStore>();
+            // Call AddEventSourcing from the EventSourcing.EF module
+            services.AddEventSourcing<ExampleContext>();
         }
         else throw new ArgumentException("No configuration found for event store", nameof(configuration));
-        
-        return services.AddScoped<IAggregateService, AggregateService>();
+
+        return services;
     }
 }
