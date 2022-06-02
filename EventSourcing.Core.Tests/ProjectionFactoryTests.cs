e29f2159bb25d652267d1960f2f8f2f303c87cf9
nielsdisveld <Niels.Disveld@finaps.nl>
2022-06-02 11:27:00 +0200

    Improve projection test

--- EventSourcing.Core.Tests/ProjectionFactoryTests.cs
+++ EventSourcing.Core.Tests/ProjectionFactoryTests.cs
@@ -5,12 +5,15 @@ public class ProjectionFactoryTests
     [Fact]
     public void Can_Project_Aggregate()
     {
-        var aggregate = new EmptyAggregate();
-        aggregate.Apply(new EmptyEvent());
-        var projection = aggregate.Project<EmptyProjection>();
+        var aggregate = new MockAggregate();
+        aggregate.Apply(new MockEvent { MockDecimal = 3.33m, MockEnum = MockEnum.B, MockNullableString = "Test"});
+        var projection = aggregate.Project<MockAggregateProjection>();
         
         Assert.NotNull(projection);
         Assert.Equal(aggregate.Id, projection.AggregateId);
         Assert.Equal(aggregate.Version, projection.Version);
+        Assert.Equal(aggregate.MockDecimal, projection.MockDecimal);
+        Assert.Equal(aggregate.MockEnum, projection.MockEnum);
+        Assert.Equal(aggregate.MockNullableString, projection.MockNullableString);
     }
 }
