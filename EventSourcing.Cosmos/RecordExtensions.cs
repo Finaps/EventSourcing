0995a79f40d3d8d26fc9fe7b9d38eee76b3b6ff5
Bram Kraaijeveld <bram.kraaijeveld@finaps.nl>
2022-07-19 10:51:23 +0200

    add record extension documentation

--- EventSourcing.Cosmos/RecordExtensions.cs
+++ EventSourcing.Cosmos/RecordExtensions.cs
@@ -2,13 +2,22 @@ using Finaps.EventSourcing.Core;
 
 namespace Finaps.EventSourcing.Cosmos;
 
+/// <summary>
+/// <see cref="Record"/> extensions
+/// </summary>
 public static class RecordExtensions
 {
+  /// <summary>
+  /// Get Cosmos Record Id 
+  /// </summary>
+  /// <param name="record"><see cref="Record"/></param>
+  /// <returns>id string</returns>
+  /// <exception cref="ArgumentException"></exception>
   public static string GetId(this Record record) => record switch
   {
     Projection p => $"{p.Kind}|{p.BaseType}|{p.AggregateId}",
     Snapshot s => $"{s.Kind}|{s.AggregateId}[{s.Index}]",
     Event e => $"{e.Kind}|{e.AggregateId}[{e.Index}]",
-    _ => throw new ArgumentException()
+    _ => throw new ArgumentException($"Invalid Record type. Record should be of type {nameof(Event)}, {nameof(Snapshot)} or {nameof(Projection)}. Found {record.GetType()}")
   };
 }
